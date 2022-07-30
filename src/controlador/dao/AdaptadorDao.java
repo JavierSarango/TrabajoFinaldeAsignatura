aab572c4cf3fb9e672e411e92d28d799f0f400a6
Jonathan Sarabia <83920444+Jonathan-sarabia@users.noreply.github.com>
2022-07-30 12:11:17 +0200

    se agrego el adaptador dao visto en clases

--- src/controlador/dao/AdaptadorDao.java
+++ src/controlador/dao/AdaptadorDao.java
@@ -5,124 +5,215 @@
  */
 package controlador.DAO;
 
-import controlador.tda.lista.ListaEnlazada;
-import controlador.tda.lista.ListaEnlazadaServices;
-import controlador.utiles.Utilidades;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import controlador.Conexion;
+import controlador.tda.lista.ListaEnlazada;
+import controlador.utiles.Utilidades;
+import static controlador.utiles.Utilidades.getMethod;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author Nathaly
  */
 public class AdaptadorDao<T> implements InterfazDao<T> {
-
-    private Class<T> clazz;
-    private  String URL = "datos" + File.separatorChar;
-
-    public AdaptadorDao(Class<T> clazz) {
+    
+    private Connection conexion;
+    private Class clazz;
+    private String ALL = "select * from ";
+    private String ALL_ID = "select * from ";
+    
+    public AdaptadorDao(Class clazz) {
         this.clazz = clazz;
-        URL += this.clazz.getSimpleName() + ".xml";
+        this.conexion = Conexion.getConecction();
+        ALL += clazz.getSimpleName().toLowerCase();
+        ALL_ID += clazz.getSimpleName().toLowerCase() + " where id = ";
     }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public Class getClazz() {
+        return clazz;
+    }
+
 
     @Override
     public void guardar(T dato) throws Exception {
-        ListaEnlazada<T> lista = listar().getLista();
-        if(lista.getSize() != 0) {
-            lista.insertar(dato, lista.getSize() - 1);
-        } else {
-            lista.insertar(dato, 0);
+           String[] columnas = columnas();
+        String comando = "insert into " + clazz.getSimpleName().toLowerCase() + " ";
+        String variables = "";
+        String datos = "";
+        for (int i = 0; i < columnas.length; i++) {
+            if (i == columnas.length - 1) {
+                variables += columnas[i];//id, nombres, external_id, ...
+                datos += tipoDato(columnas[i], dato);//0, "casa", "343-545
+            } else {
+                variables += columnas[i] + " , ";
+                datos += tipoDato(columnas[i], dato) + " , ";
+            }
         }
-        
-        FileOutputStream file = new FileOutputStream(URL);
-        JAXBContext jAXBContext = JAXBContext.newInstance(new Class[]{ListaEnlazada.class, this.clazz});
-        Marshaller marshaller = jAXBContext.createMarshaller();
+        comando += "(" + variables + ") value(" + datos + ")";
+        try {
+            PreparedStatement stmt = getConexion().prepareStatement(comando);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Error en guardar " + e);
+        }
 
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-        marshaller.marshal(lista, file);
+        System.out.println(comando);
     }
 
     @Override
-    public void modificar(T dato, Integer pos) throws Exception {
-        ListaEnlazada<T> lista = listar().getLista();
-        lista.modificarDato(pos, dato);
-        FileOutputStream file = new FileOutputStream(URL);
-        JAXBContext jAXBContext = JAXBContext.newInstance(new Class[]{ListaEnlazada.class, this.clazz});
-        Marshaller marshaller = jAXBContext.createMarshaller();
+    public void modificar(T dato) throws Exception {
+        //objeto a modificar ya lo envian desde el metodo
+        //reemplazar por valores nuevos
+        //enviar el comando
+        String[] columnas = columnas();
+        String comando = "update  " + clazz.getSimpleName().toLowerCase() + " SET ";
+        //String variables = "";
+        Object id = null;
+        String datos = "";
+        for (int i = 0; i < columnas.length; i++) {
+            if (!columnas[i].equalsIgnoreCase("id")) {
+                if (i == columnas.length - 1) {
+                    //variables += columnas[i];//id, nombres, external_id, ...
+                    datos += columnas[i] + "=" + tipoDato(columnas[i], dato);//0, "casa", "343-545
+                } else {
+                    //variables += columnas[i] + " , ";
+                    datos += columnas[i] + "=" + tipoDato(columnas[i], dato) + " , ";//0, "casa", "343-545
+                }
+            } else {
+                id = tipoDato(columnas[i], dato);
+            }
 
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        }
+        comando += datos + " where id = " + id.toString();
+        try {
+            PreparedStatement stmt = getConexion().prepareStatement(comando);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Error en guardar " + e);
+        }
 
-        marshaller.marshal(lista, file);
+        System.out.println(comando);
+
     }
 
     @Override
-    public ListaEnlazadaServices<T> listar() {
+    public ListaEnlazada<T> listar() {
         ListaEnlazada<T> lista = new ListaEnlazada<>();
         try {
-            //DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();//valido para jdk 1.8
-            DocumentBuilder db = dbf.newDocumentBuilder();
-           // FileInputStream xmlFile = new FileInputStream(URL);   
-            
-                    
-            //ByteArrayInputStream bis = new ByteArrayInputStream(xmlFile.readAllBytes());
-            //Document doc = db.parse(bis);
-            Document doc = db.parse(new File(URL));//valido para jdk 1.8
-            NodeList datos = doc.getElementsByTagName(this.clazz.getSimpleName().toLowerCase());
-            for (int i = 0; i < datos.getLength(); i++) {
-                Node n1 = datos.item(i);
 
-                NodeList aux = n1.getChildNodes();
-                T objeto = this.clazz.newInstance();
-                
-                for (int j = 0; j < aux.getLength(); j++) {
-                    Node dato = aux.item(j);
+            PreparedStatement stmt = getConexion().prepareStatement(ALL);
+            ResultSet resultSet = stmt.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            String[] columna = new String[resultSetMetaData.getColumnCount()];
+            for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                columna[i] = resultSetMetaData.getColumnLabel(i + 1);
+            }
 
-                    if (dato.getNodeName() != null && !dato.getNodeName().equals("")
-                            && dato.getTextContent() != null && !dato.getTextContent().equals("") && !dato.getNodeName().equals("#text")) {
-                        
-                        objeto = (T) Utilidades.cambiarDatos(dato.getTextContent(), dato.getNodeName(), objeto);
-                        
+            while (resultSet.next()) {
+                T obj = (T) clazz.getConstructor().newInstance();
+                for (int i = 0; i < columna.length; i++) {
+                    Object objeto = resultSet.getObject(i + 1);
+                    System.out.println("** " + objeto + columna[i]);
+                    if (objeto != null && objeto.getClass().getName().equals("java.sql.Timestamp")) {
+                        java.sql.Timestamp aux = (java.sql.Timestamp) objeto;
+                        java.util.Date fecha = new Date(aux.getTime());
+                        Utilidades.cambiarDatos(fecha, columna[i], obj);
+                    } else {
+                        Utilidades.cambiarDatos(objeto, columna[i], obj);
                     }
 
                 }
-                lista.insertar(objeto, lista.getSize() - 1);
-
+                lista.insertarCabecera(obj);
             }
-            lista.imprimir();
+
         } catch (Exception e) {
-            System.out.println("NO SE PUEDE CARGAR "+e);
-            //e.printStackTrace();
+            System.out.println("Error al cargar " + e);
+            e.printStackTrace();
         }
-        ListaEnlazadaServices<T> listado = new ListaEnlazadaServices<>();
-        listado.setLista(lista);
-        System.out.println("Hola listado");
-        return listado;
+
+        return lista;
     }
-    
-    public boolean eliminarDatos(int pos) throws Exception {
-        ListaEnlazadaServices<T> aux = listar();
-        try {
-            for (int i = 0; i < aux.getSize(); i++) {
-                if (pos == i) {
-                    aux.eliminarPosicion(i);
+
+    @Override
+    public T obtener(Integer id) throws Exception {
+       T obj = null;
+        String[] columna = columnas();
+        PreparedStatement stmt = getConexion().prepareStatement(ALL_ID + id.toString());
+        ResultSet resultSet = stmt.executeQuery();
+        while (resultSet.next()) {
+            obj = (T) clazz.getConstructor().newInstance();
+            for (int i = 0; i < columna.length; i++) {
+                Object objeto = resultSet.getObject(i + 1);
+                System.out.println("** " + objeto + columna[i]);
+                if (objeto != null && objeto.getClass().getName().equals("java.sql.Timestamp")) {
+                    java.sql.Timestamp aux = (java.sql.Timestamp) objeto;
+                    java.util.Date fecha = new Date(aux.getTime());
+                    Utilidades.cambiarDatos(fecha, columna[i], obj);
+                } else {
+                    Utilidades.cambiarDatos(objeto, columna[i], obj);
                 }
             }
-            return true;
+        }
+        return obj;
+    }
+    
+    
+    private String[] columnas() {
+        String[] columna = null;
+        try {
+            String seleccion = ALL;
+            PreparedStatement stmt = getConexion().prepareStatement(seleccion);
+            ResultSet resultSet = stmt.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            columna = new String[resultSetMetaData.getColumnCount()];
+            for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                columna[i] = resultSetMetaData.getColumnLabel(i + 1);
+            }
+        } catch (Exception ex) {
+
+        }
+        return columna;
+    }
+
+    private Object tipoDato(String name, T objeto) {
+        String aux = "";
+        try {
+            Field field = Utilidades.getField(name, clazz);
+            char[] arr = name.toCharArray();
+            arr[0] = Character.toUpperCase(arr[0]);
+            Method method = getMethod("get" + new String(arr), clazz);
+            Object dato = method.invoke(objeto);
+
+            if (field.getType().getSimpleName().equals("Date")) {
+                SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                aux = (dato != null) ? "\"" + formato.format((Date) dato) + "\"" : "null";
+            } else if (field.getType().getSuperclass().getSimpleName().equalsIgnoreCase("Number")) {
+                aux = (dato != null) ? dato.toString() : "0";
+            } else if (field.getType().getSimpleName().equals("Boolean")) {
+                aux = (dato != null) ? dato.toString() : Boolean.FALSE.toString();
+            } else {
+                aux = (dato != null) ? "\"" + dato + "\"" : "null";
+            }
         } catch (Exception e) {
-            return false;
+            System.out.println("xxxx " + e);
+
         }
 
-}
+        return aux;
+    }
+
+    
+ 
 }
