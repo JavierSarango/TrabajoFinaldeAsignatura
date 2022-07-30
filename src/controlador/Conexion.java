746cc3fc056e661b828095e0ac392937a940c736
Jonathan Sarabia <83920444+Jonathan-sarabia@users.noreply.github.com>
2022-07-30 12:10:46 +0200

    se agrego la clase de coneccion con Mysql

---
+++ src/controlador/Conexion.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Javier Sarabia
+ */
+public class Conexion {
+
+    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static String IP = "127.0.0.1";
+    private static String PORT = "3306";
+    private static String USER = "desarrollo";
+    private static String PASSWORD = "desarrollo";
+    private static String DATABASE = "desarrollo";
+    public static Connection conecction;
+
+    public static Connection getConecction() {
+        if (conecction == null) {
+            conectar();
+        }
+        return conecction;
+    }
+
+    private static Connection conectar() {
+
+        try {
+            Class.forName(DRIVER);
+            String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+            conecction = (Connection) DriverManager.getConnection(url, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Error en conexion " + e);
+        }
+        return conecction;
+    }
+
+    public static void main(String[] args) {
+        Connection cn = Conexion.getConecction();
+        try {
+            System.out.println(cn.getCatalog());
+        } catch (Exception e) {
+        }
+
+    }
+}
