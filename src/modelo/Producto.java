3320985d2ae62d537cd6ac1351ff24272cbb60b1
59396 <59396@LenovoDiegoM>
2022-08-05 07:55:55 -0500

    Diego Márquez Arreglos modeloProducto

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -15,109 +15,17 @@ public class Producto implements Serializable{
     private Long id;
     private String nombre;
     private String descripcion; 
-    private Double precioIngresado;
-    private Double preciosiniva;
-    private Double precioconiva;
-    private Boolean incluyeIva;
-    private Boolean productolibredeImpuestos;
+    private Double precioCompra;
+    private Double precioVenta; 
     private int unidades;
     private Boolean visible;
     
-    
-    /**
-     *Genera losdistintos geters y seters de la información
-     */
-    public void setIncluyeIva(Boolean incluyeIva) {
-        
-        this.incluyeIva = incluyeIva;
-    }
-
-    public Boolean getVisible() {
-        if(visible==null){
-            return true;
-        }
-        
-        return visible;
-    }
-
-    public void setVisible(Boolean visible) {
-        this.visible = visible;
-    }
- 
-
-
-    public Boolean getIncluyeIva() {
-        return incluyeIva;
-    }
-    
-    
-
-    public int getUnidades() {
-        return unidades;
-    }
-
-    public void setUnidades(int unidades) {
-        this.unidades = unidades;
-    }
-
-    public Double getPrecioIngresado() {
-        return precioIngresado;
-    }
-
-    public void setPrecioIngresado(Double precioIngresado) {
-        this.precioIngresado = precioIngresado;
-    }
-    
-   
-    
-       public Double getPrecioconiva() {
-        return precioconiva;
-    }
-
-    public void setPrecioconiva(Double precioconiva, Double iva) {
-       
-            if (getIncluyeIva() == true || getProductolibredeImpuestos() == true) {
-                this.precioconiva = precioconiva;
-               
-            } else {
-                this.precioconiva=precioconiva+(precioconiva/100)*iva;
-                
-            }
-        
-    }
-
-    public void setPreciosiniva(Double preciosiniva) {
-        this.preciosiniva = preciosiniva;
-    }
-
-    public void setPrecioconiva(Double precioconiva) {
-        this.precioconiva = precioconiva;
-    }
-
-    public Boolean getProductolibredeImpuestos() {
-        return productolibredeImpuestos;
-    }
-
-    public void setProductolibredeImpuestos(Boolean productolibredeImpuestos) {
-        this.productolibredeImpuestos = productolibredeImpuestos;
-    }
-   
-
-    public Long getId() {
+    public Long getId() {    
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
     }
 
     public String getNombre() {
@@ -128,30 +36,57 @@ public class Producto implements Serializable{
         this.nombre = nombre;
     }
 
-    public Double getPreciosiniva() {
-        return preciosiniva;
+    public String getDescripcion() {
+        return descripcion;
     }
-    
-     
-    public void setPreciosiniva(Double preciosiniva, Double iva) {
-        if (getIncluyeIva() == true || getProductolibredeImpuestos() == true) {
-                
-               this.preciosiniva=preciosiniva-(preciosiniva/100)*iva;
-        } else {
-               this.preciosiniva = preciosiniva; 
-                
-        }
-        
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
+
+    public Double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(Double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    /**
+     *Genera losdistintos geters y seters de nuestro modelo roducto
+     */
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+ 
     public Producto() {
     }
 
-    public Producto(String nombre, String descripcion, Double precioIngresado, Boolean incluyeIva, Boolean productolibredeImpuestos) {
+    public Producto(String nombre, String descripcion, Double precioCompra, Boolean incluyeIva, Double precioVenta) {
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.precioIngresado = precioIngresado;
-        this.incluyeIva = incluyeIva;
-        this.productolibredeImpuestos = productolibredeImpuestos;
+        this.precioCompra = precioCompra; 
+        this.precioVenta = precioVenta;
     }
     
     
@@ -160,13 +95,10 @@ public class Producto implements Serializable{
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.preciosiniva = preciosiniva;
-        this.precioconiva = precioconiva;
-   
-        this.productolibredeImpuestos = productolibredeImpuestos;
-    }
-
+        this.precioVenta = preciosiniva;
+        this.precioCompra = precioCompra;
     
+    }
 
     @Override
     public String toString() {
