03164b8054c0cdba918c20a17f0af8c881deaf0f
Jordy Chuquimarca <jordy.chuquimarca@unl.edu.ec>
2022-08-12 00:47:53 -0500

    Jordy Modificación controlador vista bbd

---
+++ src/controlador/dao/ClienteDao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.dao;
+
+import modelo.Cliente;
+
+/**
+ *
+ * @author Pc
+ */
+public class ClienteDao extends AdaptadorDao<Cliente> {
+
+    private Cliente cliente;
+
+    public ClienteDao() {
+        super(Cliente.class);
+    }
+
+    public Cliente getCliente() {
+        if (cliente == null) {
+            cliente = new Cliente();
+        }
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Boolean guardar() {
+        try {
+            guardar(this.getCliente());
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en guardar o modificar");
+            return false;
+        }
+    }
+
+}
