94fdc5a44e66f46d714b3eb567ecd787234fb3ad
Jonathan Sarabia <jonathan.sarabia@unl.edu.ec>
2022-08-13 16:37:48 -0500

    no funciona el metodo modificar

--- src/controlador/dao/EquipoElectronicoDao.java
+++ src/controlador/dao/EquipoElectronicoDao.java
@@ -4,8 +4,12 @@
  */
 package controlador.dao;
 
+import controlador.Conexion;
 import controlador.dao.AdaptadorDao;
 import controlador.tda.lista.ListaEnlazada;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import modelo.equipo;
 import modelo.Proveedor;
 
@@ -17,7 +21,6 @@ public class EquipoElectronicoDao extends AdaptadorDao<equipo> {
 
     private equipo equipoElectronico;
     private ListaEnlazada<Proveedor> listaequipos;
-    
 
     public equipo getEquipoElectronico() {
         if (equipoElectronico == null) {
@@ -35,13 +38,12 @@ public class EquipoElectronicoDao extends AdaptadorDao<equipo> {
 
     }
 
-     public Boolean guardar_modificar(){
+    public Boolean guardar_modificar() {
         try {
-            if (equipoElectronico.getId_equipo()!= null) {
+            if (equipoElectronico.getId_equipo() != null) {
                 modificaree(this.getEquipoElectronico());
-                
-                
-            }else{
+
+            } else {
                 guardar(this.getEquipoElectronico());
             }
             return true;
@@ -50,10 +52,11 @@ public class EquipoElectronicoDao extends AdaptadorDao<equipo> {
             return false;
         }
     }
+
     public Boolean guardar() {
-        try {         
-                guardar(this.getEquipoElectronico());
-            
+        try {
+            guardar(this.getEquipoElectronico());
+
             return true;
         } catch (Exception e) {
             System.out.println("Error en guardar o modificar");
@@ -61,5 +64,31 @@ public class EquipoElectronicoDao extends AdaptadorDao<equipo> {
         }
     }
 
+    public Boolean modificarManualCliente() {
+        PreparedStatement pst;
+        Connection con = Conexion.getConecction();
+        String sql = ("UPDATE equipo SET razon_social =?, "
+                + "tipo_equipo =?, marca =?, modelo =?, "
+                + "cargador =?, estado_ingreso =?, descripcion_problema =?,precio_servicio =? WHERE id_equipo =?");
+        try {
+            pst = (PreparedStatement) con.prepareStatement(sql);
+//            pst.setInt(1, equipoElectronico.getId_equipo());
+            pst.setString(2, equipoElectronico.getRazon_social());
+            pst.setString(3, equipoElectronico.getTipo_equipo().toString());
+            pst.setString(4, equipoElectronico.getMarca());
+            pst.setString(5, equipoElectronico.getModelo());
+            pst.setBoolean(6, equipoElectronico.getCargador());
+            pst.setString(7, equipoElectronico.getEstado_ingreso());
+            pst.setString(8, equipoElectronico.getDescripcion_problema());
+            pst.setDouble(9, equipoElectronico.getPrecio_servicio());
+            pst.executeUpdate();
+            pst.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return false;
+        }
+
+    }
 
 }
