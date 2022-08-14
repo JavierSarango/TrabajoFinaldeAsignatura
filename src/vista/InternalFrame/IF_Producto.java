acea6ecb973baf5900c4ef38aced4ffb1742fe0c
clk55 <oswaldo.m.paccha@unl.edu.ec>
2022-08-14 17:44:02 -0500

    metodo listarProveedores arreglos estéticos Vista Productos

--- src/vista/InternalFrame/IF_Producto.java
+++ src/vista/InternalFrame/IF_Producto.java
@@ -5,9 +5,12 @@
 package vista.InternalFrame;
 
 import controlador.dao.ProductoDao;
+import controlador.dao.ProveedorDao;
 import controlador.services.ServicioProducto;
+import controlador.tda.lista.ListaEnlazada;
 import controlador.utiles.enums.TipoOrdenacion;
 import javax.swing.JOptionPane;
+import modelo.Proveedor;
 import vista.ModeloTablas.ModeloTablaProducto;
 
 /**
@@ -19,12 +22,14 @@ public class IF_Producto extends javax.swing.JInternalFrame {
     private ModeloTablaProducto mtp = new ModeloTablaProducto();
     private ServicioProducto sp = new ServicioProducto();
     private ProductoDao productoDao = new ProductoDao();
+    private ProveedorDao proovedorDao = new ProveedorDao();
     /**
      * Creates new form IF_Producto
      */
     public IF_Producto() {
         initComponents();
         limpiar();
+        cargarProveedor();
 
         buttonGroup1.add(radioA);
         buttonGroup1.add(radioD);
@@ -53,9 +58,22 @@ private void cargarTabla() {
         cbx_datoBuscar.setSelectedIndex(0);
         cargarTabla();
     }
+    
+    private void cargarProveedor() {
+        cbx_proveedor.removeAllItems();
+        ListaEnlazada<Proveedor> listap = proovedorDao.listarIDProveedor();
+        for (int i = 0; i < listap.getSize(); i++) {
+            try {
+                cbx_proveedor.addItem(listap.obtenerDato(i).toString());
+            } catch (Exception e) {
+                System.out.println("ingreso" + i + "Proveedor");
+            }
+            cbx_proveedor.updateUI();
+        }
+    }
 
     private void guardar() throws Exception {
-        if (txt_codigo.getText().trim().isEmpty() || txt_nombre.getText().trim().isEmpty()
+         if (txt_codigo.getText().trim().isEmpty() || txt_nombre.getText().trim().isEmpty()
                 || txt_descripcion.getText().trim().isEmpty() || txt_precioCompra.getText().trim().isEmpty()
                 || txt_precioVenta.getText().trim().isEmpty() || txt_unidades.getText().trim().isEmpty()) {
             JOptionPane.showMessageDialog(null, "Porfavor llenar todos los campos requeridos", "Insertar Datos", JOptionPane.WARNING_MESSAGE);
@@ -67,6 +85,7 @@ private void cargarTabla() {
             sp.getProducto().setUnidades(Integer.parseInt(txt_unidades.getText()));
             sp.getProducto().setPrecioCompra(Double.parseDouble(txt_precioCompra.getText()));
             sp.getProducto().setPrecioVenta(Double.parseDouble(txt_precioVenta.getText()));
+            sp.getProducto().setProveedor(cbx_proveedor.getSelectedItem().toString());
 //        sp.getProducto().setUpdatedAt(updateAt);
 
             if (sp.guardar_modificar()) {
@@ -166,9 +185,17 @@ private void cargarTabla() {
 
     private void buscar() throws Exception {
         String criterioBusqueda = cbx_datoBuscar.getSelectedItem().toString().toLowerCase();
-        String datoBusqueda = txt_busqueda.getText().trim().toLowerCase();
+        if (cbx_datoBuscar.getSelectedIndex() == 0) {
+            Integer datoBusqueda = Integer.parseInt(txt_busqueda.getText());
+            mtp.setLista(sp.listar().buscarDatoPosicionObjetoBinaria(criterioBusqueda, datoBusqueda));
+            
+        }else{
+            String datoBusqueda = txt_busqueda.getText().trim().toLowerCase();
+             mtp.setLista(sp.listar().buscarDatoPosicionObjetoBinaria(criterioBusqueda, datoBusqueda));
+        }
+              
 
-        mtp.setLista(sp.listar().buscarDatoPosicionObjetoBinaria(criterioBusqueda, datoBusqueda));
+        
         System.out.println("se realizo busqueda");
 
         tbl_producto.setModel(mtp);
@@ -264,7 +291,7 @@ private void cargarTabla() {
         jPanel2.add(jScrollPane1);
         jScrollPane1.setBounds(10, 100, 1040, 160);
 
-        bnt_eliminar.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        bnt_eliminar.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         bnt_eliminar.setText("ELIMINAR");
         bnt_eliminar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -272,9 +299,9 @@ private void cargarTabla() {
             }
         });
         jPanel2.add(bnt_eliminar);
-        bnt_eliminar.setBounds(310, 20, 110, 28);
+        bnt_eliminar.setBounds(310, 20, 110, 19);
 
-        btn_cargarrrr.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        btn_cargarrrr.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         btn_cargarrrr.setText("cargar");
         btn_cargarrrr.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -282,9 +309,9 @@ private void cargarTabla() {
             }
         });
         jPanel2.add(btn_cargarrrr);
-        btn_cargarrrr.setBounds(600, 50, 84, 28);
+        btn_cargarrrr.setBounds(600, 50, 84, 19);
 
-        btn_modd.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        btn_modd.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         btn_modd.setText("modd");
         btn_modd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -292,7 +319,7 @@ private void cargarTabla() {
             }
         });
         jPanel2.add(btn_modd);
-        btn_modd.setBounds(580, 20, 72, 28);
+        btn_modd.setBounds(580, 20, 72, 19);
 
         jPanel1.add(jPanel2);
         jPanel2.setBounds(10, 300, 1060, 270);
@@ -300,17 +327,17 @@ private void cargarTabla() {
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "AGREGAR PRODUCTOS"));
         jPanel3.setLayout(null);
 
-        jLabel7.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel7.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel7.setText("Código:");
         jPanel3.add(jLabel7);
         jLabel7.setBounds(10, 20, 60, 18);
 
-        jLabel8.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel8.setText("Nombre:");
         jPanel3.add(jLabel8);
-        jLabel8.setBounds(110, 20, 60, 22);
+        jLabel8.setBounds(110, 20, 60, 14);
         jPanel3.add(txt_nombre);
-        txt_nombre.setBounds(110, 40, 130, 24);
+        txt_nombre.setBounds(110, 40, 130, 22);
 
         txt_codigo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -318,9 +345,9 @@ private void cargarTabla() {
             }
         });
         jPanel3.add(txt_codigo);
-        txt_codigo.setBounds(10, 40, 90, 24);
+        txt_codigo.setBounds(10, 40, 90, 22);
 
-        jButton3.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jButton3.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         jButton3.setText("AGREGAR");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -328,21 +355,21 @@ private void cargarTabla() {
             }
         });
         jPanel3.add(jButton3);
-        jButton3.setBounds(270, 140, 90, 28);
+        jButton3.setBounds(270, 140, 90, 19);
         jPanel3.add(txt_precioCompra);
-        txt_precioCompra.setBounds(10, 90, 90, 24);
+        txt_precioCompra.setBounds(10, 90, 90, 22);
 
-        jLabel9.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel9.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel9.setText("Precio Compra:");
         jPanel3.add(jLabel9);
-        jLabel9.setBounds(10, 70, 110, 22);
+        jLabel9.setBounds(10, 70, 110, 14);
 
-        jLabel10.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel10.setText("Descripción:");
         jPanel3.add(jLabel10);
-        jLabel10.setBounds(250, 20, 100, 22);
+        jLabel10.setBounds(250, 20, 100, 14);
         jPanel3.add(txt_descripcion);
-        txt_descripcion.setBounds(250, 40, 150, 24);
+        txt_descripcion.setBounds(250, 40, 150, 22);
 
         txt_precioVenta.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -350,23 +377,28 @@ private void cargarTabla() {
             }
         });
         jPanel3.add(txt_precioVenta);
-        txt_precioVenta.setBounds(110, 90, 130, 24);
+        txt_precioVenta.setBounds(110, 90, 130, 22);
 
-        jLabel11.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel11.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel11.setText("Precio Venta:");
         jPanel3.add(jLabel11);
-        jLabel11.setBounds(110, 70, 110, 22);
+        jLabel11.setBounds(110, 70, 110, 14);
 
-        jLabel12.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel12.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel12.setText("Proveedor:");
         jPanel3.add(jLabel12);
         jLabel12.setBounds(110, 120, 80, 20);
 
         cbx_proveedor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbx_proveedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbx_proveedorActionPerformed(evt);
+            }
+        });
         jPanel3.add(cbx_proveedor);
-        cbx_proveedor.setBounds(110, 140, 130, 24);
+        cbx_proveedor.setBounds(110, 140, 130, 22);
 
-        check_automatico.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        check_automatico.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         check_automatico.setText("Precio Automatico");
         check_automatico.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -376,12 +408,12 @@ private void cargarTabla() {
         jPanel3.add(check_automatico);
         check_automatico.setBounds(260, 90, 130, 19);
 
-        jLabel14.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel14.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel14.setText("Unidades:");
         jPanel3.add(jLabel14);
         jLabel14.setBounds(10, 120, 70, 20);
         jPanel3.add(txt_unidades);
-        txt_unidades.setBounds(10, 140, 90, 24);
+        txt_unidades.setBounds(10, 140, 90, 22);
 
         jPanel1.add(jPanel3);
         jPanel3.setBounds(10, 110, 420, 180);
@@ -389,14 +421,14 @@ private void cargarTabla() {
         jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "BUSCAR PRODUCTOS"));
         jPanel4.setLayout(null);
 
-        jLabel13.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel13.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel13.setText("Dato a Buscar:");
         jPanel4.add(jLabel13);
         jLabel13.setBounds(10, 70, 120, 18);
         jPanel4.add(txt_busqueda);
-        txt_busqueda.setBounds(10, 90, 180, 24);
+        txt_busqueda.setBounds(20, 90, 130, 22);
 
-        jButton2.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jButton2.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         jButton2.setText("BUSCAR");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -404,18 +436,19 @@ private void cargarTabla() {
             }
         });
         jPanel4.add(jButton2);
-        jButton2.setBounds(10, 140, 80, 28);
+        jButton2.setBounds(40, 120, 80, 19);
 
-        jLabel21.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel21.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel21.setText("Criterio de Busqueda:");
         jPanel4.add(jLabel21);
         jLabel21.setBounds(10, 20, 120, 18);
 
+        cbx_datoBuscar.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         cbx_datoBuscar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Codigo", "Nombre", "Proveedor" }));
         jPanel4.add(cbx_datoBuscar);
-        cbx_datoBuscar.setBounds(10, 40, 130, 24);
+        cbx_datoBuscar.setBounds(20, 40, 130, 20);
 
-        jButton4.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jButton4.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
         jButton4.setText("RECARGAR TABLA");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -423,24 +456,25 @@ private void cargarTabla() {
             }
         });
         jPanel4.add(jButton4);
-        jButton4.setBounds(100, 140, 122, 28);
+        jButton4.setBounds(20, 150, 122, 19);
 
         jPanel1.add(jPanel4);
-        jPanel4.setBounds(440, 110, 240, 180);
-        jPanel4.getAccessibleContext().setAccessibleName("BUSCAR PRODUCTOS");
+        jPanel4.setBounds(440, 110, 170, 180);
 
         jPanel5.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "ORDENAR PRODUCTOS"));
         jPanel5.setLayout(null);
 
-        jLabel22.setFont(new java.awt.Font("NATS", 1, 14)); // NOI18N
+        jLabel22.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel22.setText("Criterio de Ordenación:");
         jPanel5.add(jLabel22);
         jLabel22.setBounds(10, 20, 150, 18);
 
+        cbx_datoOrdenar.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         cbx_datoOrdenar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Codigo", "Nombre", "Proveedor" }));
         jPanel5.add(cbx_datoOrdenar);
-        cbx_datoOrdenar.setBounds(10, 40, 130, 24);
+        cbx_datoOrdenar.setBounds(10, 40, 130, 20);
 
+        radioA.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         radioA.setText("Ascendentemente");
         radioA.addItemListener(new java.awt.event.ItemListener() {
             public void itemStateChanged(java.awt.event.ItemEvent evt) {
@@ -453,8 +487,9 @@ private void cargarTabla() {
             }
         });
         jPanel5.add(radioA);
-        radioA.setBounds(50, 90, 150, 22);
+        radioA.setBounds(10, 70, 130, 20);
 
+        radioD.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         radioD.setText("Descendetemente");
         radioD.addItemListener(new java.awt.event.ItemListener() {
             public void itemStateChanged(java.awt.event.ItemEvent evt) {
@@ -472,10 +507,10 @@ private void cargarTabla() {
             }
         });
         jPanel5.add(radioD);
-        radioD.setBounds(50, 120, 150, 22);
+        radioD.setBounds(10, 100, 130, 19);
 
         jPanel1.add(jPanel5);
-        jPanel5.setBounds(690, 110, 240, 180);
+        jPanel5.setBounds(620, 110, 160, 180);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -586,6 +621,10 @@ private void cargarTabla() {
         // TODO add your handling code here:
         cargarTabla();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void cbx_proveedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbx_proveedorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbx_proveedorActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
